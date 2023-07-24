82b189a0ae696351813e7ed0533b96fc317113ab
Comarlau Vlad <vladcom95@gmail.com>
2023-07-24 21:35:41 +0300

    - add remote micro API for controlling the set-top box using IR box - add UI interface for selecting channels

---
+++ tvStreamer/src/main/java/org/example/credentials.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class credentials {
+    private static String userName = "comarlau";
+    private static String passWord = "44Streamer55%";
+    public static String getUserName(){
+        return userName;
+    }
+    public static String getPassWord(){
+        return passWord;
+    }
+}
